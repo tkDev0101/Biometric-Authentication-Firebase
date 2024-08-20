3e9f4ef8a65edb91dde8c25ed35c2084fda47e35
tkDev <153438920+tkDev0101@users.noreply.github.com>
2024-08-20 12:33:58 +0200

    Implemented drawable resource files for page design

---
+++ .fingerprintauthentication/app/src/main/java/com/tk/fingerprintauthentication/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.tk.fingerprintauthentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+
+    //VARIABLES
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Assign Variables
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        //Event Handler
+        loginBtn.setOnClickListener()
+        {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username: $username and Password: $password")
+
+        }
+
+
+    }
+}
